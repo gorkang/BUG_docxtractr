c3eb46562a3a266deca5556f215cfb3a08df5f41
Gorka Navarrete <gorkang@gmail.com>
2022-08-03 13:08:31 +0100

    detailed issue

--- bug.R
+++ bug.R
@@ -1,10 +1,11 @@
 reprex::reprex({
-DOC = docxtractr::read_docx(here::here("~/Downloads/test.docx"))
+  
+DOC = docxtractr::read_docx(here::here("test.docx"))
+TABLE = docxtractr::docx_extract_tbl(DOC, preserve = TRUE, header = FALSE)
 
-docxtractr::docx_describe_tbls(DOC)
-docxtractr::docx_extract_all(DOC)
-docxtractr::docx_extract_tbl(DOC)
-docxtractr::docx_extract_tbl(DOC, tbl_number = 1, preserve = TRUE, header = FALSE) %>% View
-}, input = "/home/emrys/Downloads/TEST/BUG_docxtractr")
-# 
-# DF_ALL %>% fill(...1, .direction = "down") %>% View
+# Not using a numbered list. All is fine
+TABLE$V2[2]
+
+# In row 3 we use a numbered list. Numbers are lost
+TABLE$V2[3]
+})
